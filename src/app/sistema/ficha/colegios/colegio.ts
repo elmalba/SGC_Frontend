bcdc0f527949515b75b1a76bfe0e10329c43b8f6
Matias Vera <matias.veraaravena@gmail.com>
2016-12-05 01:06:37 -0300

    primera versión EditarColegios

--- src/app/sistema/ficha/colegios/colegio.ts
+++ src/app/sistema/ficha/colegios/colegio.ts
@@ -21,4 +21,5 @@ export class Colegio {
   sostenedor: string;
   rbd: string;
   dependencia: string;
+
 }
--- src/app/sistema/ficha/colegios/editar-colegio/editar-colegio.component.ts
+++ src/app/sistema/ficha/colegios/editar-colegio/editar-colegio.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 import { Location }       from '@angular/common';
+import { ColegiosService } from '../../colegios.service';
+import { Colegio } from '../colegio';
 
 @Component({
   selector: 'app-editar-colegio',
@@ -8,20 +10,39 @@ import { Location }       from '@angular/common';
   styleUrls: ['./editar-colegio.component.css']
 })
 export class EditarColegioComponent implements OnInit {
+
   id: number;
   private sub: any;
 
+  selectedColegio: Colegio;
+  colegio: Colegio;
+
   constructor(
       private route: ActivatedRoute,
-      private location: Location
+      private location: Location,
+      private colegiosService: ColegiosService,
   ) { }
 
   ngOnInit() {
+    this.selectedColegio = new Colegio();
+    this.colegio = new Colegio();
+
     this.sub = this.route.params.subscribe( params => { this.id = +params['id'];});
+
+    this.route.params
+      .switchMap((params: Params) => this.colegiosService.getColegio(+params['id']))
+      .subscribe((colegio) => {this.selectedColegio = colegio;this.colegio = this.selectedColegio});
+
   }
 
   goBack(): void {
     this.location.back();
+  }
+
+  saveColegio(): void {
+    console.log(this.selectedColegio);
+    console.log(this.colegio);
+    this.colegiosService.updateColegio(this.colegio);
   }
 
 
